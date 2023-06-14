908cb5851bd95bc8839d240a232b0a6ebb4d160c
HJ S <hjseong1222@gmail.com>
2023-06-14 10:02:58 +0900

    0614 리액트에서 변수 주석

--- react-hook/src/components/InputBox.jsx
+++ react-hook/src/components/InputBox.jsx
@@ -20,6 +20,16 @@ export default function InputBox() {
     setName(nameProps.value);
   }
 
+  // useState나 props값이 아닌 컴포넌트 안의 변수
+  // 값이 수정된 값으로 고정되지 않고 (렌더/리렌더시 let testNum = 1; 실행)
+  // 화면에 바뀐 값이 출력이 되지 않으면서 (값이 바뀌어도 렌더/리렌더X)
+  // 렌더/리렌더 되면 다시 1이 된다
+  // 컴포넌트 안에서 순수한 변수값(1, [1,2])은 잘 쓰지않는다
+  let testNum = 1;
+  
+  // State나, props의 값을 수정, 일부분의 내용을 가져올때 사용
+  const nameLength = name.length;
+
   return (
     <div>
         <label htmlFor="">
